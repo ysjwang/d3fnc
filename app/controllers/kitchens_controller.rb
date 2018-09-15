cd1f742b6f899870e28a4b0efffa29142d1a7575
Josiah <ysjwang@gmail.com>
2018-09-14 23:43:21 -0400

    Added code blue

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -27,11 +27,16 @@ class KitchensController < ApplicationController
 
 		distance_threshold = 1.00 # set this as the distance threshold miles, to include search results for. This can be tweaked as needed
 
+		# puts "Begin params"
+		# puts JSON.pretty_generate(params[:result][:metadata])
+		# puts "End params"
+
+		intent_name = params[:result][:metadata][:intentName].to_s
 
 		meal_type = params[:result][:parameters][:meal_type].to_s
 		location = params[:result][:parameters][:location].values.first.to_s
 
-		puts "Meal type came back as #{meal_type}"
+		puts "Intent was #{intent_name}"
 
 
 		calculated_meal_type_by_time = case Time.now.hour
@@ -44,45 +49,80 @@ class KitchensController < ApplicationController
 
 		parsed_meal_type = ['breakfast', 'lunch', 'dinner'].include?(meal_type) ? meal_type : calculated_meal_type_by_time
 
-		formatted_response = "I didn't understand your query"
+		formatted_response = "I didn't understand your query."
 
 
-		if parsed_meal_type == 'none'
-			formatted_response = "It's too late right now to find food."
-		elsif location.blank?
-			puts "We got into here for #{parsed_meal_type}"
-			formatted_response = "Where are you looking for a #{parsed_meal_type}?"
+		my_coordinates = get_coordinates(location)
+
+
+
+		if intent_name == 'code_blue'
+
 			
-		else
-
-			my_coordinates = get_coordinates(location)
-			airtable_results = parse_airtable(parsed_meal_type, my_coordinates, distance_threshold)
+			formatted_response = "I couldn't find a place with an open bed."
+			airtable_results = parse_airtable('code_blue', my_coordinates, distance_threshold)
 			primary_result = airtable_results.first
 
-			location_name, location_address, location_distance, location_subway_line, location_subway_stop = [
+			location_name, location_address, location_distance, location_subway_line, location_subway_stop, code_blue_capacity = [
 				primary_result['fields']['name'],
 				primary_result['fields']['address'],
 				primary_result['distance'].round(2),
 				primary_result['fields']['subway_lines'],
-				primary_result['fields']['subway_stop']
+				primary_result['fields']['subway_stop'],
+				(primary_result['fields']['code_blue_capacity'].to_f * 100).round.to_i
 			]
 
 
-			formatted_response = "It sounds like you are looking for #{parsed_meal_type} today near #{location.titlecase.strip}. \n\n" \
-			"The nearest place for #{parsed_meal_type} from there is #{location_name.titlecase.strip}, " \
+			formatted_response = "It sounds like you are looking for an open shelter near #{location.titlecase.strip}. \n\n" \
+			"The nearest place with Code Blue support from there is #{location_name.titlecase.strip}, " \
 			"at #{location_address.titlecase.strip}, about #{location_distance} miles from #{location.titlecase.strip}. \n\n" \
+			"There are currently #{code_blue_capacity} beds open. \n\n" \
 			"The closest subway station is the #{location_subway_stop.titlecase.strip} stop, on the #{location_subway_line.strip} train. \n\n" \
 			"There are #{airtable_results.count - 1} other results within #{distance_threshold} miles of #{location.titlecase.strip}."
 
-			formatted_response
+
+
+		else
+
+
+			if parsed_meal_type == 'none'
+				formatted_response = "It's too late right now to find food."
+			elsif location.blank?
+				formatted_response = "Where are you looking for a #{parsed_meal_type}?"
+				
+			else
+
+				
+				airtable_results = parse_airtable(parsed_meal_type, my_coordinates, distance_threshold)
+				primary_result = airtable_results.first
+
+				location_name, location_address, location_distance, location_subway_line, location_subway_stop = [
+					primary_result['fields']['name'],
+					primary_result['fields']['address'],
+					primary_result['distance'].round(2),
+					primary_result['fields']['subway_lines'],
+					primary_result['fields']['subway_stop']
+				]
+
+
+				formatted_response = "It sounds like you are looking for #{parsed_meal_type} today near #{location.titlecase.strip}. \n\n" \
+				"The nearest place for #{parsed_meal_type} from there is #{location_name.titlecase.strip}, " \
+				"at #{location_address.titlecase.strip}, about #{location_distance} miles from #{location.titlecase.strip}. \n\n" \
+				"The closest subway station is the #{location_subway_stop.titlecase.strip} stop, on the #{location_subway_line.strip} train. \n\n" \
+				"There are #{airtable_results.count - 1} other results within #{distance_threshold} miles of #{location.titlecase.strip}."
+
+
+			end
 		end
 
-		@response = {
-			speech: formatted_response,
-			displayText: formatted_response
-		}
+			@response = {
+				speech: formatted_response,
+				displayText: formatted_response
+			}
 
-		json_response(@response)
+			json_response(@response)
+
+		
 
 	end
 
@@ -94,9 +134,7 @@ class KitchensController < ApplicationController
 
 	def get_coordinates(location)
 		location = location + ", new york city"
-		puts "Trying to get coordinates for #{location}"
 		coordinates = Geocoder.coordinates(location)
-		puts "Coordinates for #{location} are #{coordinates.to_s}"
 		return coordinates
 	end
 
@@ -105,12 +143,11 @@ class KitchensController < ApplicationController
 
 
 
-	def parse_airtable(meal_type, my_coordinates, distance_threshold=1)
+	def parse_airtable(page_type, my_coordinates, distance_threshold=1)
 
 
-		puts "Attempting to find #{meal_type} near #{my_coordinates}"
 
-		airtable_json = Kitchen.grab_airtable(meal_type)
+		airtable_json = Kitchen.grab_airtable(page_type)
 
 
 		airtable_json['records'].each do |record|
@@ -121,7 +158,6 @@ class KitchensController < ApplicationController
 		sorted_results = airtable_json['records'].select{|record| record['distance'].to_f < distance_threshold}.sort_by{|record| record['distance'].to_f}
 
 
-		puts JSON.pretty_generate(sorted_results)
 
 		return sorted_results
 
